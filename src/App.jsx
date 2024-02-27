53c5ec64459c65cedb600898134c4680fd18f82a
Daniela Eichner <daniela.eichner@nytimes.com>
2024-02-27 16:20:47 -0500

    up

---
+++ src/App.jsx
@@ -0,0 +1,229 @@
+import { useState } from "react";
+import "./App.css";
+import {childHold, parentHold, resetHold, setHoldStatus } from "./childHold";
+import { createTree} from "./createTree";
+
+
+function App() {
+  const [list, setList] = useState([]);
+
+
+
+const [tree, setTree] = useState(createTree());
+
+  const handleClick = (parent) => {
+    setList([]);
+    // loop until you find the list array element
+    const newList = [];
+
+    const createFileList = (parent, newList) => {
+      if(parent.children) {
+        parent.children.forEach((child) => {
+          createFileList(child,newList)
+        
+        });
+      } else {
+        if(parent.list) {
+          newList.push(...parent.list);
+        }
+      }
+    }
+
+    createFileList(parent, newList);
+    setList(newList);
+  };
+
+
+  const toggleOnHold = (id) => {
+
+    const updateTree = (tree) => {
+      if (tree.id === id) {  
+        tree.onHold = !tree.onHold;
+      } 
+        if (tree.children) {
+          for (let i = 0; i < tree.children.length; i++) {
+            updateTree(tree.children[i]);
+          }
+        }
+        if (tree.list) {
+          for (let i = 0; i < tree.list.length; i++) {
+            updateTree(tree.list[i]);
+          }
+        }
+    };
+
+    const newTree = { ...tree };
+    updateTree(newTree);
+    setTree(newTree);
+
+
+    resetHold(newTree, setTree);
+
+    childHold(newTree, setTree);
+    parentHold(newTree, setTree);
+
+    setHoldStatus(newTree, setTree);
+    
+  };
+ 
+
+  const fileStatusOptions = ["Planned", "Processed", "Sent"];
+
+
+const getStatusColorSingle = (onHold,  status, parentHold ) => {
+  if(status === 'Sent') {
+    return 'green';
+  }
+  if(status === 'Error') {
+    return 'red';
+  }
+
+  if(status === 'Processed' && (onHold || parentHold)) {
+    return 'blue';
+  }
+
+  if(status === 'Planned' && (onHold || parentHold)) {
+    return 'yellow';
+  }
+};
+
+const getStatusColor = (child) => {
+
+  const {  onHold,
+    areAllOnHold,
+    partialChildrenOnHold,
+    allPlanned,
+    allProcessed,
+    allSent,
+    processedAndSent,	
+    processedAndPlanned,
+    sentAndPlanned,
+    allMixed} = child;
+
+  
+  if(allSent) {
+    return 'green';
+  }
+
+  if(partialChildrenOnHold && (allPlanned ||sentAndPlanned)) {
+    return 'lightyellow';
+  }
+  if((areAllOnHold || onHold) && (allPlanned || sentAndPlanned)) {
+    return 'yellow';
+  }
+  if((partialChildrenOnHold || areAllOnHold || onHold) && (allMixed || processedAndPlanned)) {
+    return 'lightblue';
+  }
+
+  if((partialChildrenOnHold || areAllOnHold) && (processedAndSent || allProcessed)) {
+    return 'blue';
+  }
+
+  if(allMixed) {
+    return 'lightgreen';
+  }
+  return 'white';
+
+};
+
+
+const changeStatus = (id, status) => {
+  const updateTree = (tree) => {
+    if (tree.id === id) {  
+      tree.fileStatus = status;
+    } 
+      if (tree.children) {
+        for (let i = 0; i < tree.children.length; i++) {
+          updateTree(tree.children[i]);
+        }
+      }
+      if (tree.list) {
+        for (let i = 0; i < tree.list.length; i++) {
+          updateTree(tree.list[i]);
+        }
+      }
+  };
+
+  const newTree = { ...tree };
+  updateTree(newTree);
+  setTree(newTree);
+  setHoldStatus(newTree, setTree);
+
+};
+
+  const spanElement = (child, onClickEl) => {
+    return (
+      <span 
+          style={{backgroundColor: child.fileStatus ? getStatusColorSingle(child.onHold, child.fileStatus, child.parentHold) : getStatusColor(child)}}
+            onClick={onClickEl ? () => handleClick(child) : undefined}
+          >{child.name} <span style={{fontSize: "0.5em"}}>{child.onHold? 'onHold' : ''} {child.childHold? ' - childOnHold' : ''} 
+          {child.parentHold? ' - parentOnHold' : ''}
+          {child.areAllOnHold? ' - areAllOnHold' : ''}
+          {child.partialChildrenOnHold? ' - partialChildrenOnHold' : ''}
+          </span>
+          <button onClick={() => toggleOnHold(child.id)}>
+            {child.onHold ? 'Unhold' : 'Hold'}
+            </button>
+            
+            {child.fileStatus ?
+
+
+            <select value={child.fileStatus}  style={{marginLeft: "1rem"}} onChange={(e) => changeStatus(child.id, e.target.value)}>
+                  {fileStatusOptions.map((option) => (
+                    <option key={option} value={option} defaultValue={child.fileStatus} >
+                      {option}
+                    </option>
+                  ))}
+                </select>
+            : null}
+      </span>
+
+    );
+
+  }
+
+ 
+  const renderTree = (tree) => {
+    return (
+      <ul>
+        <li>
+          {" "}
+          {spanElement(tree,true)}
+        </li>
+        {tree.children && tree.children.length > 0 && (
+          <ul>
+            {tree.children.map((child) => (
+              <li key={child.name + child.id}>{renderTree(child)}</li>
+            ))}
+          </ul>
+        )}
+      </ul>
+    );
+  };
+
+  return (
+    <>
+      <h1>STX Status Colors</h1>
+
+      <div className="container">
+        <div className="leftTree">{renderTree(tree)}</div>
+        <div className="fileList">
+          <ul>
+            {list.map((child) => (
+              <li key={child.name + child.id}>
+                {spanElement(child, false)}
+               
+                
+               
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/createTree.js
@@ -0,0 +1,221 @@
+
+export const createTree = () => {
+
+return{
+  name: "Friday, Feb 23rd 2024",
+  id: "1",
+  children: [
+    {
+      name: "TEST National",
+      id: "2",
+      children: [
+        {
+          name: "EARLY NY",
+          id: "3",
+          children: [
+            {
+              name: "E1",
+              id: "4",
+              children: [
+                {
+                  name: "C",
+                  id: "5",
+                  list: [
+                    {
+                      name: "022324 - TEST National -  EARLY NY - E1 - C - 01",
+                      id: "6",
+                      fileStatus: "Processed",
+                    },
+                    {
+                      name: "022324 - TEST National -  EARLY NY - E1 - C - 02",
+                      id: "7",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  EARLY NY - E1 - C - 03",
+                      id: "8",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  EARLY NY - E1 - C - 04",
+                      id: "9",
+                      fileStatus: "Planned",
+                    },
+                  ],
+                },
+                {
+                  name: "D",
+                  id: "10",
+                  list: [
+                    {
+                      name: "022324 - TEST National -  EARLY NY - E1 - D - 01",
+                      id: "11",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  EARLY NY - E1 - D - 02",
+                      id: "12",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  EARLY NY - E1 - D - 03",
+                      id: "13",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  EARLY NY - E1 - D - 04",
+                      id: "14",
+                      fileStatus: "Planned",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: "LATE NY E1",
+          id: "15",
+          children: [
+            {
+              name: "E1",
+              id: "16",
+              children: [
+                {
+                  name: "A",
+                  id: "17",
+                  list: [
+                    {
+                      name: "022324 - TEST National -  LATE NY E1 - E1 - A - 01",
+                      id: "18",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  LATE NY E1 - E1 - A - 02",
+                      id: "19",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  LATE NY E1 - E1 - A - 03",
+                      id: "20",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  LATE NY E1 - E1 - A - 04",
+                      id: "21",
+                      fileStatus: "Planned",
+                    },
+                  ],
+                },
+                {
+                  name: "B",
+                  id: "22",
+                  list: [
+                    {
+                      name: "022324 - TEST National -  LATE NY E1 - E1 - B - 01",
+                      id: "23",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  LATE NY E1 - E1 - B - 02",
+                      id: "24",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  LATE NY E1 - E1 - B - 03",
+                      id: "25",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  LATE NY E1 - E1 - B - 04",
+                      id: "26",
+                      fileStatus: "Planned",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: "Master Daily TEST",
+          id: "27",
+          children: [
+            {
+              name: "E1",
+              id: "28",
+              children: [
+                {
+                  name: "A",
+                  id: "29",
+                  list: [
+                    {
+                      name: "022324 - TEST National -  Master Daily TEST - E1 - A - 01",
+                      id: "30",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  Master Daily TEST - E1 - A - 02",
+                      id: "31",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  Master Daily TEST - E1 - A - 03",
+                      id: "32",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  Master Daily TEST - E1 - A - 04",
+                      id: "33",
+                      fileStatus: "Planned",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: "Master Daily TEST PDF_PDF",
+          id: "34",
+          children: [
+            {
+              name: "E1",
+              id: "35",
+              children: [
+                {
+                  name: "A",
+                  id: "36",
+                  list: [
+                    {
+                      name: "022324 - TEST National -  Master Daily TEST PDF_PDF - E1 - A - 01",
+                      id: "37",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  Master Daily TEST PDF_PDF - E1 - A - 02",
+                      id: "38",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  Master Daily TEST PDF_PDF - E1 - A - 03",
+                      id: "39",
+                      fileStatus: "Planned",
+                    },
+                    {
+                      name: "022324 - TEST National -  Master Daily TEST PDF_PDF - E1 - A - 04",
+                      id: "40",
+                      fileStatus: "Planned",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+}
